21a19ecec0436dffea21e125c6cb1d7793caec6a
Yorky3321 <magicyork666@gmail.com>
2026-01-09 00:19:25 +0800

    commit -a "magic_square"

--- Data_construct_1.cpp
+++ Data_construct_1.cpp
@@ -13,33 +13,23 @@ int main(int argc, char const *argv[])
         cin>>n;
     }
     for (int i = 1; i <= n; i += 2) {
+        cout << endl;
         int r = 0;
         int c = (i + 1) / 2 - 1;
         vector<vector<int>> A(i, vector<int>(i, 0));
         A[r][c] = 1;
         for (int y = 2; y <= i * i; ++y) {
+            if (r == 0) r = i - 1;
+            else --r;
+            if (c == i - 1) c = 0;
+            else ++c;
             if (A[r][c] != 0) {
-                if (r == 0) {
-                    r = i - 1;
-                } else {
-                    --r;
-                }
-                if (c == i - 1) {
-                    c = 0;
-                } else {
-                    ++c;
-                }
-            } else {
-                if (r == i - 1) {
-                    r = 0;
-                } else {
-                    r++;
-                }
-                if (c == 0) {
-                    c = i - 1;
-                } else {
-                    c--;
-                }
+                if (r == i - 1) r = 0;
+                else r ++;
+                if (c == 0) c = i - 1;
+                else c --;
+                if (r == i - 1) r = 0;
+                else r ++;
             }
             A[r][c] = y;
         }
@@ -49,7 +39,6 @@ int main(int argc, char const *argv[])
             }
             cout << endl;
         }
-        cout << endl;
     }
     return 0;
 }
