79345cbe1fe8bfb8712e9f43ad19961e2d2869e0
Yorky3321 <magicyork666@gmail.com>
2026-01-15 01:14:51 +0800

    binary_End

--- Binary search/Binary_search.cpp
+++ Binary search/Binary_search.cpp
@@ -1,31 +1,64 @@
 #include <iostream>
 using namespace std;
 
+struct SearchResult {
+    int index;
+    int iterations;
+};
 
 class Search {
     public:
-        Search(int array[], int number) {
+        Search(int array[], int sizeofarr, int target) {
             arr = &array[0];
-            num = number;
+            tar = target;
+            size = sizeofarr;
+            count = 0;
         }
 
-        int B_search() {
+        SearchResult B_search() {
             int left = 0;
-            int right = arr.size() -1;
-            int mid = arr.size() / 2;
-            while ((right - left) > 2) {
-                if (arr[mid] == num) {
-                    return mid;
-                } else if (arr[mid] < num) {
-                    left = mid;
-                    mid = (right + left) / 2;
-                } else if (arr[mid] > num) {
-                    right = mid;
+            int right = size - 1;
+            int mid = size / 2 - 1;
+            while (1) {
+                count++;
+                if (left == right) {
+                    if (arr[left] != tar) return {-1, 0}; 
+                    return {left, count};
+                }else if (arr[mid] < tar) {
+                    left = mid + 1;
+                    mid = (right + left)  / 2;
+                } else if (arr[mid] > tar) {
+                    right = mid - 1;
                     mid = (right + left) / 2;
                 }
             }
         }
     private:
         int* arr;               /* the pointer preventing allocate particular array in the memory */
-        int num;
+        int tar;
+        int size;
+        int count;
+};
+
+
+
+int main() {
+    int arr[] = {3, 6, 10, 11, 12, 13, 14, 15, 17, 19, 20, 22, 25, 30};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 30;
+
+    SearchResult res = Search(arr, size, target).B_search(); 
+    int result = res.index;
+    int count = res.iterations;
+
+    if (result != -1) {
+        cout << "Element found at index: " << result << endl;
+        cout << "Number of iterations: " << count << endl;
+    } else {
+        cout << "Element not found in the array." << endl;
+    }
+
+    cout << size << endl;
+
+    return 0;
 }
