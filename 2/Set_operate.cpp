df2d1b1a9e6314c443d96f18e9ba4d3fbddee1f2
Yorky3321 <magicyork666@gmail.com>
2026-01-24 15:58:21 +0800

    HW2_Pre_Work

--- 2/Set_operate.cpp
+++ 2/Set_operate.cpp
@@ -4,14 +4,28 @@ using namespace std;
 
 class Tset {
     public:
-        Tset(char *d, char *c, char s) {
-            for (int i = 0; i < 256; i++) {
-                data[i] = d[i];
-                comp[i] = c[i];
+        Tset(char *d, char s) {
+            for (int i = 0; i < 256 && d[i] != '\0'; i++) data[i] = d[i];
+            special = s;
+        }
+        void display() {
+            for (int i = 0; i < 256 && data[i] != '\0'; i++) {
+                cout << data[i] << " ";
             }
+            cout << endl;
         }
     private:
-        char data[256];
-        char comp[256];
+        char data[256] = {0};
         char special;
+};
+
+int main() {
+    cout << "Set Operations :" << endl;
+    char us[256], cs[256];
+    cin >> us >> cs;
+    Tset A(us, 0);
+    Tset B(cs, 0);
+    A.display();
+    B.display();
+    return 0;
 }
